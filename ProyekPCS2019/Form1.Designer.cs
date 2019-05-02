8ca4cfe33d38dfa631ce777f3f57fdfb851c7c8b
Billy Gondokusuma <billy.gondokusuma@hotmail.com>
2019-05-02 22:28:56 +0700

    buat form awal dan mecah untuk tiap sektor

--- ProyekPCS2019/Form1.Designer.cs
+++ ProyekPCS2019/Form1.Designer.cs
@@ -28,20 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(290, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(222, 90);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Master";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(290, 169);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(222, 90);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Front Office";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(290, 283);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(222, 90);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Client";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(312, 376);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "tolong nanti diganti ya :3";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(680, 417);
+            this.ClientSize = new System.Drawing.Size(780, 444);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Select Mode";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
     }
 }
 
