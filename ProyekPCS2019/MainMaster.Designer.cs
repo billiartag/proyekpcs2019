dc34d1db284efc3a4ad24072df94f2907e4e13bc
edwin sidharta <edwin0sidharta@gmail.com>
2019-05-07 16:05:13 +0700

    Benarkan database

--- ProyekPCS2019/MainMaster.Designer.cs
+++ ProyekPCS2019/MainMaster.Designer.cs
@@ -35,8 +35,8 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1234, 650);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            //this.Controls.Add(this.label2);
+            //this.Controls.Add(this.label1);
             this.Name = "MainMaster";
             this.Text = "MainMaster";
             this.Load += new System.EventHandler(this.MainMaster_Load);
