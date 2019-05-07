33d559e2c155404caaa30b71cb359f91d9dae137
alfonsus_yves <alfonsus3@mhs.stts.edu>
2019-05-07 15:53:40 +0700

    Ganti nama form Login jadi LoginKaryawan

--- ProyekPCS2019/MainClient.Designer.cs
+++ ProyekPCS2019/MainClient.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(138, 119);
+            this.button1.Location = new System.Drawing.Point(277, 129);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -44,7 +44,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(726, 480);
             this.Controls.Add(this.button1);
             this.Name = "MainClient";
             this.Text = "MainClient";
